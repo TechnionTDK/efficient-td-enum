f40378a0aba47338cbf34aa14ce88c82c6741347
dvir.dukhan <dvir.dukhan@campus.technion.ac.il>
2017-08-28 13:29:48 +0300

    moved comparators to new package

--- src/main/java/tdenum/graph/data_structures/WeightedQueue.java
+++ src/main/java/tdenum/graph/data_structures/WeightedQueue.java
@@ -4,88 +4,20 @@ import javafx.util.Pair;
 
 import java.util.*;
 
+
+import tdenum.graph.data_structures.comparators.ValueComparator;
+import tdenum.graph.data_structures.comparators.ValueComparatorDesc;
+
 /**
  * Created by dvir.dukhan on 7/11/2017.
  */
 public  class WeightedQueue<T >
 {
 
-    class ValueComparatorDesc implements Comparator
-    {
-        Map<T, Integer> map;
-        ValueComparatorDesc(Map<T, Integer> map)
-        {
-            this.map = map;
-        }
 
 
-        @Override
-        public int compare(Object o1, Object o2) {
-            if (!o1.equals(o2))
-            {
-                Pair e1 = (Pair) o1;
-                Pair e2 = (Pair) o2;
-                Integer val2 = map.get(e2.getKey());
-                Integer val1 = map.get(e1.getKey());
-                int valComp = val2.compareTo(val1);
-                if (valComp == 0)
-                {
-                    if (!e2.getKey().equals(e1.getKey()))
-                    {
-                        int keyComp = ((Integer)e1.getKey().hashCode()).compareTo(e2.getKey().hashCode());
-                        if (keyComp == 0)
-                        {
-                            return -1;
-                        }
-                        return keyComp;
-                    }
-
-                }
-                return  valComp;
-            }
-            return 0;
-
-        }
-    }
 
 
-    class ValueComparator implements Comparator
-    {
-        Map<T, Integer> map;
-        ValueComparator(Map<T, Integer> map)
-        {
-            this.map = map;
-        }
-
-
-        @Override
-        public int compare(Object o1, Object o2) {
-            if (!o1.equals(o2))
-            {
-                Pair e1 = (Pair) o1;
-                Pair e2 = (Pair) o2;
-                Integer val2 = map.get(e2.getKey());
-                Integer val1 = map.get(e1.getKey());
-                int valComp = val1.compareTo(val2);
-                if (valComp == 0)
-                {
-                    if (!e1.getKey().equals(e2.getKey()))
-                    {
-                        int keyComp = ((Integer)e1.getKey().hashCode()).compareTo(e2.getKey().hashCode());
-                        if (keyComp == 0)
-                        {
-                            return -1;
-                        }
-                        return keyComp;
-                    }
-
-                }
-                return  valComp;
-            }
-            return 0;
-
-        }
-    }
 
     Map<T, Integer> data =new HashMap<>();
     TreeSet<Pair<T, Integer>> queue = new TreeSet<>(new ValueComparator(data));
